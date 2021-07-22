06e78eba1c2d3d8d77dea5d36baa70dd1967dbe3
Haibert Barfian <31780749+haibert@users.noreply.github.com>
2021-07-21 23:00:49 -0700

    Update index.js
    added haptics, and also used worklet function to run passed function on js thread.

--- index.js
+++ index.js
@@ -1,63 +1,61 @@
-import React from "react";
-import { View, StyleSheet } from "react-native";
+import React from 'react'
+import { View, StyleSheet, TouchableWithoutFeedback } from 'react-native'
 import Animated, {
-  useSharedValue,
-  useAnimatedStyle,
-  useAnimatedGestureHandler,
-  withSpring,
-} from "react-native-reanimated";
-import { LongPressGestureHandler } from "react-native-gesture-handler";
-
+    useSharedValue,
+    useAnimatedStyle,
+    runOnJS,
+    withSpring,
+} from 'react-native-reanimated'
+import * as Haptics from 'expo-haptics'
 const ScaleButton = ({
-  children,
-  onPress = () => {},
-  activeScale = 0.9,
-  springConfig = {
-    damping: 10,
-    mass: 1,
-    stiffness: 200,
-  },
-  contentContainerStyle,
-  handlerProps,
+    haptics,
+    children,
+    onPress = () => {},
+    activeScale = 0.9,
+    springConfig = {
+        damping: 20,
+        mass: 1,
+        stiffness: 500,
+    },
+    contentContainerStyle,
+    animatedStyle,
 }) => {
-  const scale = useSharedValue(1);
-  const sz = useAnimatedStyle(() => {
-    return {
-      transform: [
-        {
-          scale: withSpring(scale.value, springConfig),
-        },
-      ],
-    };
-  });
-
-  const gestureHandler = useAnimatedGestureHandler({
-    onStart: () => {
-      scale.value = activeScale;
-    },
-    onCancel: () => {
-      scale.value = 1;
-    },
-    onEnd: () => {
-      onPress();
-      scale.value = 1;
-    },
-  });
-
-  return (
-    <LongPressGestureHandler
-      minDurationMs={0.5}
-      maxDist={10}
-      {...handlerProps}
-      onGestureEvent={gestureHandler}
-    >
-      <Animated.View style={[sz, contentContainerStyle]}>
-        {children}
-      </Animated.View>
-    </LongPressGestureHandler>
-  );
-};
-
-const s = StyleSheet.create();
-
-export default ScaleButton;
+    const scale = useSharedValue(1)
+    const sz = useAnimatedStyle(() => {
+        return {
+            transform: [
+                {
+                    scale: scale.value,
+                },
+            ],
+        }
+    })
+    function runOnJSPlease() {
+        'worklet'
+        runOnJS(onPress)()
+    }
+    const animateIn = () => {
+        scale.value = activeScale
+    }
+    const animateOut = () => {
+        scale.value = withSpring(1, springConfig)
+    }
+    return (
+        <TouchableWithoutFeedback
+            onPress={() => {
+                runOnJSPlease()
+                if (haptics) {
+                    Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Light)
+                }
+            }}
+            onPressIn={animateIn}
+            onPressOut={animateOut}
+        >
+            <Animated.View style={[sz, contentContainerStyle, animatedStyle]}>
+                {children}
+            </Animated.View>
+        </TouchableWithoutFeedback>
+    )
+}
+const s = StyleSheet.create()
+export default ScaleButton
